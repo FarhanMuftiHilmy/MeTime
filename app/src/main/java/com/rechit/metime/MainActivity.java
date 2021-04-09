96ce41bdad7b5ead9a5b501af781596852601ba9
Farhan Mufti Hilmy <farhanmuftihilmy@gmail.com>
2021-04-09 21:40:34 +0700

    Add Splash Screen

--- app/src/main/java/com/rechit/metime/MainActivity.java
+++ app/src/main/java/com/rechit/metime/MainActivity.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.rechit.metime.R;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+        setTheme(R.style.Theme_MeTime);
         setContentView(R.layout.activity_main);
     }
 }
