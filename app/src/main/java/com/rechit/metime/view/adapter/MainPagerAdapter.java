7d523aba133f1d57374d82630790caaef6ee2155
Farhan Mufti Hilmy <farhanmuftihilmy@gmail.com>
2021-04-11 14:54:57 +0700

    Add bottom navigation

---
+++ app/src/main/java/com/rechit/metime/view/adapter/MainPagerAdapter.java
@@ -0,0 +1,43 @@
+package com.rechit.metime.view.adapter;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import com.rechit.metime.view.ui.CalendarFragment;
+import com.rechit.metime.view.ui.DashboardFragment;
+import com.rechit.metime.view.ui.NoteFragment;
+import com.rechit.metime.view.ui.TimeFragment;
+
+public class MainPagerAdapter extends FragmentPagerAdapter {
+    public MainPagerAdapter(@NonNull FragmentManager fm, int behavior) {
+        super(fm, behavior);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        Fragment fragment = null;
+        switch (position){
+            case 0:
+                fragment = new DashboardFragment();
+                break;
+            case 1:
+                fragment = new CalendarFragment();
+                break;
+            case 2:
+                fragment = new TimeFragment();
+                break;
+            case 3:
+                fragment = new NoteFragment();
+                break;
+        }
+        return fragment;
+    }
+
+    @Override
+    public int getCount() {
+        return 4;
+    }
+}
--- app/src/main/java/com/rechit/metime/view/ui/MainActivity.java
+++ app/src/main/java/com/rechit/metime/view/ui/MainActivity.java
@@ -1,17 +1,63 @@
 package com.rechit.metime.view.ui;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.rechit.metime.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setTheme(R.style.Theme_MeTime);
         setContentView(R.layout.activity_main);
+
+        loadFragment(new DashboardFragment());
+        setBottomNavigationView();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Fragment fragment = null;
+        switch(item.getItemId()){
+            case R.id.dashboard_menu:
+                fragment = new DashboardFragment();
+                break;
+            case R.id.calendar_menu:
+                fragment = new CalendarFragment();
+                break;
+            case R.id.time_menu:
+                fragment = new TimeFragment();
+                break;
+            case R.id.note_menu:
+                fragment = new NoteFragment();
+                break;
+        }
+        return loadFragment(fragment);
+    }
+
+    private boolean loadFragment(Fragment fragment) {
+        if(fragment!=null){
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fl_container, fragment)
+                    .addToBackStack(null)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
+
+    public void setBottomNavigationView() {
+        bottomNavigationView = findViewById(R.id.bn_main);
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
     }
 }
+
