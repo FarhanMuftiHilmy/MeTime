c585de4219e19d2caae55a0d5532238e574e8d38
Farhan Mufti Hilmy <farhanmuftihilmy@gmail.com>
2021-06-01 14:12:01 +0700

    Add notes

---
+++ app/src/main/java/com/rechit/metime/activity/NoteEditorActivity.java
@@ -0,0 +1,72 @@
+package com.rechit.metime.activity;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.rechit.metime.R;
+import com.rechit.metime.view.ui.MainActivity;
+import com.rechit.metime.view.ui.NoteFragment;
+
+import java.util.HashSet;
+
+public class NoteEditorActivity extends AppCompatActivity {
+
+    int noteId;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_note_editor);
+
+
+        EditText editText = findViewById(R.id.editText);
+
+        // Fetch data that is passed from MainActivity
+        Intent intent = getIntent();
+
+        // Accessing the data using key and value
+        noteId = intent.getIntExtra("noteId", -1);
+        if (noteId != -1) {
+            editText.setText(NoteFragment.notes.get(noteId));
+        } else {
+
+            NoteFragment.notes.add("");
+            noteId = NoteFragment.notes.size() - 1;
+            NoteFragment.arrayAdapter.notifyDataSetChanged();
+
+        }
+
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                // add your code here
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                NoteFragment.notes.set(noteId, String.valueOf(charSequence));
+                NoteFragment.arrayAdapter.notifyDataSetChanged();
+                // Creating Object of SharedPreferences to store data in the phone
+                SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences("com.example.notes", Context.MODE_PRIVATE);
+                HashSet<String> set = new HashSet(NoteFragment.notes);
+
+
+                sharedPreferences.edit().putStringSet("notes", set).apply();
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                // add your code here
+            }
+        });
+    }
+}
--- app/src/main/java/com/rechit/metime/view/ui/NoteFragment.java
+++ app/src/main/java/com/rechit/metime/view/ui/NoteFragment.java
@@ -1,14 +1,39 @@
 package com.rechit.metime.view.ui;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.view.Gravity;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+import android.widget.TextView;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 import com.rechit.metime.R;
+import com.rechit.metime.activity.NoteEditorActivity;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.HashSet;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -17,14 +42,8 @@ import com.rechit.metime.R;
  */
 public class NoteFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    public static ArrayList<String> notes = new ArrayList<>();
+    public static ArrayAdapter arrayAdapter;
 
     public NoteFragment() {
         // Required empty public constructor
@@ -42,25 +61,112 @@ public class NoteFragment extends Fragment {
     public static NoteFragment newInstance(String param1, String param2) {
         NoteFragment fragment = new NoteFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
 
+
+
+
+
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        setHasOptionsMenu(true);
+
+
     }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_note, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull @NotNull View view, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        GridView gridView = getView().findViewById(R.id.gridView);
+        FloatingActionButton fab = getView().findViewById(R.id.fab);
+
+        SharedPreferences sharedPreferences = getActivity().getApplicationContext().getSharedPreferences("com.example.notes", Context.MODE_PRIVATE);
+        HashSet<String> set = (HashSet<String>) sharedPreferences.getStringSet("notes", null);
+
+        if (set == null) {
+
+            notes.add("Example note");
+        } else {
+            notes = new ArrayList(set);
+        }
+
+        // Using custom listView Provided by Android Studio
+        arrayAdapter = new ArrayAdapter(getActivity(), android.R.layout.simple_expandable_list_item_1, notes){
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent){
+                // Cast the grid view current item as a text view
+                TextView tv_cell = (TextView) super.getView(position,convertView,parent);
+
+                // Set the item background color
+                tv_cell.setBackground(getResources().getDrawable(R.drawable.notes_border));
+
+
+
+                // Put item item text in cell center
+                tv_cell.setGravity(Gravity.CENTER);
+
+                // Return the modified item
+                return tv_cell;
+            }
+        };
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getContext(), NoteEditorActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        gridView.setAdapter(arrayAdapter);
+
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                // Going from MainActivity to NotesEditorActivity
+                Intent intent = new Intent(getActivity(), NoteEditorActivity.class);
+                intent.putExtra("noteId", i);
+                startActivity(intent);
+
+            }
+        });
+
+        gridView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                final int itemToDelete = i;
+                // To delete the data from the App
+                new AlertDialog.Builder(view.getContext())
+                        .setIcon(android.R.drawable.ic_dialog_alert)
+                        .setTitle("Are you sure?")
+                        .setMessage("Do you want to delete this note?")
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                notes.remove(itemToDelete);
+                                arrayAdapter.notifyDataSetChanged();
+                                SharedPreferences sharedPreferences = getActivity().getSharedPreferences("com.example.notes", Context.MODE_PRIVATE);
+                                HashSet<String> set = new HashSet(NoteFragment.notes);
+                                sharedPreferences.edit().putStringSet("notes", set).apply();
+                            }
+                        }).setNegativeButton("No", null).show();
+                return true;
+            }
+        });
+        super.onViewCreated(view, savedInstanceState);
     }
 }
